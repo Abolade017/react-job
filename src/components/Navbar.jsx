3a8883a7061e92f2bfb7d532bde4ed7d64218f3b
Adeola Tawakaltu Adenike <tawakalituadeola68@gmail.com>
2024-07-11 18:14:24 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='bg-indigo-700 border-b border-indigo-500'>
+            <div className='max-w-7xl mx-auto pl-2 sm:px-6 lg:px-8'>
+                <div className='flex justify-between items-center h-20'>
+                    <div className='flex flex-1 items-center  md:items-stretch justify-center md:justify-start'>
+                        <a href='/' className='flex flex-shrink-0 items-center mr-4'>
+                            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png' alt='' className='h-12 w-auto' />
+                            <span className='hidden md:block text-2xl fontbold ml-2'>React Jobs</span>
+                        </a>
+                        <div className='md:ml-auto'>
+                            <div className='flex space-x-2'>
+                                <a href='/' className='text-white bg-black px-3 py-2 rounded-md hover:text-white hover:bg-gray-900'>Home</a>
+                                <a href='/' className='text-white bg-black px-3 py-2 rounded-md hover:text-white hover:bg-gray-900'>Jobs</a>
+                                <a href='/' className='text-white bg-black px-3 py-2 rounded-md hover:text-white hover:bg-gray-900'>Add Job</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/ViewAllJobs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ViewAllJobs = () => {
+    return (
+        <section className='m-auto max-w-lg px-6 my-10'>
+            <a href='/jobs' className='block bg-black text-white text-center py-4 px-6 rounded-xl hover;bg-gray-700'> View All Jobs</a>
+        </section>)
+}
+
+export default ViewAllJobs
