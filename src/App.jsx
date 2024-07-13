0ffffd00d1ef437af0f1b658d0a5310f5acb53c2
Adeola Tawakaltu Adenike <tawakalituadeola68@gmail.com>
2024-07-13 16:00:14 +0100

    move vercel.json to the root folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import React from 'react'
 import HomePage from './components/pages/HomePage'
 import JobsPage from './components/pages/JobsPage'
@@ -6,6 +6,7 @@ import JobPage from './components/pages/JobPage'
 import MainLayout from './components/layouts/MainLayout'
 import NotFoundPage from './components/pages/NotFoundPage'
 const router = createBrowserRouter(createRoutesFromElements(
+
   <Route path="/" element={<MainLayout />}>
     <Route index element={<HomePage />}></Route>
     <Route path="/jobs" element={<JobsPage />}></Route>
